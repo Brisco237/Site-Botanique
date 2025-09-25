cf03799d45791ee309d5b2de45ee0196a411c2d9
kamdem Brice <kamdembrice770@gmail.com>
2025-09-25 04:37:23 +0100

    Ajout des fichier css

--- la-maison-jungle/src/components/Banner.jsx
+++ la-maison-jungle/src/components/Banner.jsx
@@ -1,6 +1,11 @@
-function Banner(){
-	const title = 'La maison jungle'
-	return <h1>{title}</h1>
+import '../styles/Banner.css';
+
+function Banner() {
+    return (
+        <div className='lmj-banner'>
+            <h1>La maison jungle</h1>
+        </div>
+    );
 }
 
 export default Banner
