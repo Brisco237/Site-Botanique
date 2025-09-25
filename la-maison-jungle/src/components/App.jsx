32a8716090f9cc3900ab8353da68d3209e46ddec
kamdem Brice <kamdembrice770@gmail.com>
2025-09-25 05:51:53 +0100

    ajout du composant ShoppingList.jsx

--- la-maison-jungle/src/components/App.jsx
+++ la-maison-jungle/src/components/App.jsx
@@ -1,11 +1,13 @@
 import Banner from './Banner.jsx';
 import Cart from './Cart.jsx';
+import ShoppingList from './ShoppingList.jsx';
 
 function App(){
 	return(
 		<div>
 			<Banner />
 			<Cart />
+			<ShoppingList />
 		</div>
 	)
 }
