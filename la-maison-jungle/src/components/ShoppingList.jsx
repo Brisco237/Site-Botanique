a21835189555abe0255cb22d17e43f31b8002059
kamdem Brice <kamdembrice770@gmail.com>
2025-09-26 18:36:07 +0100

    ajouter le logo soldes

--- la-maison-jungle/src/components/ShoppingList.jsx
+++ la-maison-jungle/src/components/ShoppingList.jsx
@@ -1,4 +1,5 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -8,14 +9,17 @@ function ShoppingList() {
 	)
 	return (
 		<div>
-			<ul>
+			<p>Catégories de plantes :</p>
+			<ul >
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}{plant.isSpecialOffer ? <div className='lmj-sales'>Soldes</div> : null}
+					</li>
 				))}
 			</ul>
 		</div>
